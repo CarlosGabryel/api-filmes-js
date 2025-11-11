618664e8fcc5f9ae45428f35b9e6d5cc8d9efd26
Carlos Gabryel <89553773+CarlosGabryel@users.noreply.github.com>
2025-11-11 01:52:58 +0000

    feat: Adiciona rota DELETE /api/filmes/:id para remover filme e testes unitários

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-console': 'off',
+    'consistent-return': 'off',
+    'no-unused-vars': 'warn'
+  },
+};
--- app.js
+++ app.js
@@ -26,6 +26,7 @@ let filmes = [
     }
 ];
 
+let proximoId = filmes.length +1;
 
 // =======================================================
 // FEATURE 1: Rota GET para obter todos os produtos (filmes)
@@ -67,10 +68,43 @@ app.post('/api/filmes', (req, res) => {
     return res.status(201).json(novoFilme);
 });
 
-// Inicializa o servidor Express
+// =======================================================
+// FEATURE 3: Rota DELETE para remover um produto (filme)
+// DELETE /api/filmes/:id
+// =======================================================
+app.delete('/api/filmes/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+
+    const index = filmes.findIndex(f => f.id ===id);
+
+    //Caso o filme não exista deve retornar o 404:
+    if(index === -1){
+        return res.status(404).json({erro: 'Filme não encontrado.'});
+    }
+
+    //Caso o filme exista, então fazemos a remoção e retorna o 204:
+    filmes.splice(index, 1);
+    return res.status(204).send();
+});
+
+// Para o ambiente de testes que agora foi implementado, estou adicionando uma função para resetar o estado
+function resetaFilmes(){
+    filmes = [
+        { 'id': 1, 'titulo': 'O Poderoso Chefão', 'ano': 1972, 'diretor': 'Francis Ford Coppola' },
+        { 'id': 2, 'titulo': 'Um Sonho de Liberdade', 'ano': 1994, 'diretor': 'Frank Darabont' },
+        { 'id': 3, 'titulo': 'Batman: O Cavaleiro das Trevas', 'ano': 2008, 'diretor': 'Christopher Nolan' }
+    ];
+    proximoId = filmes.length + 1;
+}
+
+/* istanbul ignore next */
+// Inicia o servidor Express APENAS se não estiver em ambiente de testes
+if(process.env.NODE_ENV !== 'test'){
 app.listen(port, () => {
     console.log(`API de Filmes rodando em http://localhost:${port}/`);
     console.log(`Endpoints disponíveis:`);
     console.log(` - GET  http://localhost:${port}/api/filmes`);
     console.log(` - POST http://localhost:${port}/api/filmes`);
-});
+});}
+/* istanbul ignore next */
+module.exports = {app, resetaFilmes};
